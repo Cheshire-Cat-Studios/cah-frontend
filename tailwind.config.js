bf2d39ada8bda87bd27222fa030a56daf48f8678
Mcawley38 <41105563+Mcawley38@users.noreply.github.com>
2021-11-10 13:55:15 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+	purge: [
+		'src/**/*.vue'
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			spacing: {
+				'500': '500px',
+			},
+			inset: {
+				'1/2': '50%',
+			},
+			fontSize: {
+				'2xs': '0.5rem',
+			},
+		},
+		variants: {
+			extend: {
+				negativeMargin: [
+					'hover',
+					'responsive',
+				],
+			},
+		},
+		plugins: [],
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
